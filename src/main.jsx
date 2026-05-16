cc3e589160a3a6e555a1f089f8227078bc5d2364
marcel-lima <109974211+marcel-lima@users.noreply.github.com>
2026-05-16 01:57:34 -0300

    Block pinch-zoom on iOS via gesture events

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,8 @@
+// Prevent pinch-zoom on iOS (ignores user-scalable=no since iOS 10)
+document.addEventListener('gesturestart', (e) => e.preventDefault(), { passive: false });
+document.addEventListener('gesturechange', (e) => e.preventDefault(), { passive: false });
+document.addEventListener('touchmove', (e) => { if (e.touches.length > 1) e.preventDefault(); }, { passive: false });
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
